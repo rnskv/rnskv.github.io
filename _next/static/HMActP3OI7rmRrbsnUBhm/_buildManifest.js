de2ccdf4e1ef8cb3f6444ce63e8cfaa4b2fcc185
rnskv <rnuiskov@gmail.com>
2024-12-06 23:57:02 +0300

    init

---
+++ _next/static/HMActP3OI7rmRrbsnUBhm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/39-b04df079bbfa68f3.js","static/chunks/pages/index-88adf215ba46eaf4.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
